ec6e8e76d447015dcebf7a43745f9deb0aebb14c
for-Ely <kaneki21174@gmail.com>
2024-03-11 09:06:23 +0700

    Add a comment

--- Nah.java
+++ Nah.java
@@ -1,7 +1,6 @@
-package ReportOOPbuoi1;
-
 public class Nah {
     public static void main(String[] args) {
-        System.out.println("Nah");
+        System.out.println("Nah\\");
+        /*alo1 */
     }
 }
